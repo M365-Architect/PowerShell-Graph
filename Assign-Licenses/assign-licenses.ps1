20f2797692d66e2f31f03b6e5b6e429e4b8ffe22
Andreas Hähnel <andreas.haehnel@blackmagiccloud.de>
2022-04-22 18:35:26 +0200

    Update assign-licenses.ps1
    group name change

--- Assign-Licenses/assign-licenses.ps1
+++ Assign-Licenses/assign-licenses.ps1
@@ -3,6 +3,6 @@ $ExOPlan1 = Get-MgSubscribedSku | where SkuPartnumber -eq "EXCHANGESTANDARD"
 
 #Set UsageLocation for all users that have none
 Get-MgUser -All | where { $_.UsageLocation -eq $null -and $_.UserType -eq 'Member' }  | %{Update-MgUser -UserId $_.UserPrincipalName -UsageLocation "DE" }
-$licgroup = Get-MgGroup -Filter "Displayname eq 'sec-o365'"
+$licgroup = Get-MgGroup -Filter "Displayname eq 'users-o365'"
 
 Get-MgGroupMember -GroupId $licgroup.Id -All| %{((Get-MgUser -UserId $_.Id).UserPrincipalName); Set-MgUserLicense -UserId ((Get-MgUser -UserId $_.Id).UserPrincipalName) -AddLicenses @{SkuId = $ExOPlan1.SkuId} -RemoveLicenses @()} 

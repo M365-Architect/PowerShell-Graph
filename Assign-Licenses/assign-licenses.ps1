d51b92d43a0c1566f89f355e681f620534cbef62
Andreas Hähnel <andreas.haehnel@blackmagiccloud.de>
2022-12-23 15:21:27 +0100

    update

--- Assign-Licenses/assign-licenses.ps1
+++ Assign-Licenses/assign-licenses.ps1
@@ -1,8 +1,12 @@
-Connect-Graph -Scopes User.ReadWrite.All, Organization.Read.All,Directory.ReadWrite.All
-$ExOPlan1 = Get-MgSubscribedSku | where SkuPartnumber -eq "EXCHANGESTANDARD"
+$LicenseGroupName = "users-o365"
+Connect-MgGraph -Scopes User.ReadWrite.All, Organization.Read.All,Directory.ReadWrite.All
+$ExOPlan1 = Get-MgSubscribedSku | Where-Object {$_.SkuPartnumber -eq "EXCHANGESTANDARD"}
 
 #Set UsageLocation for all users that have none
-Get-MgUser -All | where { $_.UsageLocation -eq $null -and $_.UserType -eq 'Member' }  | %{Update-MgUser -UserId $_.UserPrincipalName -UsageLocation "DE" }
-$licgroup = Get-MgGroup -Filter "Displayname eq 'users-o365'"
+Get-MgUser -All | Where-Object { $_.UsageLocation -eq $null -and $_.UserType -eq 'Member' }  | ForEach-Object{Update-MgUser -UserId $_.UserPrincipalName -UsageLocation "DE" }
+$licgroup = Get-MgGroup -Filter "Displayname eq $($LicenseGroupName)"
 
-Get-MgGroupMember -GroupId $licgroup.Id -All| %{((Get-MgUser -UserId $_.Id).UserPrincipalName); Set-MgUserLicense -UserId ((Get-MgUser -UserId $_.Id).UserPrincipalName) -AddLicenses @{SkuId = $ExOPlan1.SkuId} -RemoveLicenses @()} 
+Get-MgGroupMember -GroupId $licgroup.Id -All| ForEach-Object{
+    ((Get-MgUser -UserId $_.Id).UserPrincipalName)
+    Set-MgUserLicense -UserId ((Get-MgUser -UserId $_.Id).UserPrincipalName) -AddLicenses @{SkuId = $ExOPlan1.SkuId} -RemoveLicenses @()
+} 
